ab3cb070ea5b5bd10215ad6be2d1e80364a5b370
Kayce-joy <wakarijoyce@gmail.com>
2025-02-05 15:19:11 +0300

    Added changes

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def is_armstrong(n):
     return sum(digit ** power for digit in digits) == n
 
 def is_perfect(n):
+    if n <= 0:  
+        return False
     return sum([i for i in range(1, n) if n % i == 0]) == n
 
 @app.route('/api/classify-number', methods=['GET'])
